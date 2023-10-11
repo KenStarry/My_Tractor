be25fa18d8106b417dc52fb3305f0a53090ef4b3
KenStarry <starrycodes@gmail.com>
2023-10-11 18:57:53 +0300

    Google Maps controller

--- lib/feature_map/presentation/map_screen.dart
+++ lib/feature_map/presentation/map_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,6 +11,19 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+
+  late final Completer<GoogleMapController> _mapController;
+
+  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _mapController = Completer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
