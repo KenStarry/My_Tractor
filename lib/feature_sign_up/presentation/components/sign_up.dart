33b4b91a37ec8148836dbb53a85bbeff966592a6
KenStarry <starrycodes@gmail.com>
2023-10-13 11:56:09 +0300

    Create Account validation

--- lib/feature_sign_up/presentation/components/sign_up.dart
+++ lib/feature_sign_up/presentation/components/sign_up.dart
@@ -14,11 +14,21 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   late final AuthController _authController;
+  late final GlobalKey<FormState> _formKey;
+  late final TextEditingController _emailController;
+  late final TextEditingController _phoneController;
+  late final TextEditingController _passwordController;
+  late final TextEditingController _confirmPasswordController;
 
   @override
   void initState() {
     super.initState();
 
+    _formKey = GlobalKey<FormState>();
+    _emailController = TextEditingController();
+    _phoneController = TextEditingController();
+    _passwordController = TextEditingController();
+    _confirmPasswordController = TextEditingController();
     _authController = Get.find<AuthController>();
   }
 
@@ -41,86 +51,131 @@ class _SignUpState extends State<SignUp> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Form(
+              key: _formKey,
               child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomTextField(
-                    hint: 'Email Address',
-                    iconData: Icons.email_rounded,
-                    textStyle: Theme.of(context).textTheme.bodyMedium!,
-                    onChanged: (value) {}),
-                CustomTextField(
-                    hint: 'Phone Number',
-                    iconData: Icons.email_rounded,
-                    textStyle: Theme.of(context).textTheme.bodyMedium!,
-                    onChanged: (value) {}),
-                CustomTextField(
-                    hint: 'New Password',
-                    iconData: Icons.key_rounded,
-                    textStyle: Theme.of(context).textTheme.bodyMedium!,
-                    onChanged: (value) {}),
-                CustomTextField(
-                    hint: 'Confirm Password',
-                    iconData: Icons.key_rounded,
-                    textStyle: Theme.of(context).textTheme.bodyMedium!,
-                    onChanged: (value) {}),
-
-                //  check if they own a tractor or want to hire one
-                const Text("I want to : "),
-
-                Column(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Obx(
-                      () => CustomRadio(
-                          title: 'Hire a Tractor',
-                          value: 'Hire',
-                          groupValue: _authController.selectedUserType.value,
-                          onTap: () {
-                            _authController.setSelectedUserType(
-                                userType: 'Hire');
-                          },
-                          onChanged: (value) {
-                            _authController.setSelectedUserType(
-                                userType: value);
-                          }),
+                    CustomTextField(
+                        hint: 'Email Address',
+                        iconData: Icons.email_rounded,
+                        textStyle: Theme.of(context).textTheme.bodyMedium!,
+                        onValidate: (value) {
+                          if (value != null && value.isNotEmpty) {
+                            return null;
+                          }
+
+                          return 'Enter a valid email address';
+                        },
+                        onChanged: (value) {}),
+                    CustomTextField(
+                        hint: 'Phone Number',
+                        iconData: Icons.email_rounded,
+                        textStyle: Theme.of(context).textTheme.bodyMedium!,
+                        onValidate: (value) {
+                          if (value != null && value.isNotEmpty) {
+                            return null;
+                          }
+
+                          return 'Enter a valid phone number';
+                        },
+                        onChanged: (value) {}),
+                    CustomTextField(
+                        hint: 'New Password',
+                        iconData: Icons.key_rounded,
+                        textStyle: Theme.of(context).textTheme.bodyMedium!,
+                        isObscured: true,
+                        inputType: TextInputType.visiblePassword,
+                        controller: _passwordController,
+                        onValidate: (value) {
+                          if (value != null && value.isNotEmpty) {
+                            return null;
+                          }
+
+                          return 'Enter a valid password';
+                        },
+                        onChanged: (value) {}),
+                    CustomTextField(
+                        hint: 'Confirm Password',
+                        iconData: Icons.key_rounded,
+                        controller: _confirmPasswordController,
+                        textStyle: Theme.of(context).textTheme.bodyMedium!,
+                        isObscured: true,
+                        inputType: TextInputType.visiblePassword,
+                        onValidate: (value) {
+                          if (value == _passwordController.text) {
+                            return null;
+                          }
+
+                          return 'Passwords do not match';
+                        },
+                        onChanged: (value) {}),
+
+                    //  check if they own a tractor or want to hire one
+                    const Text("I want to : "),
+
+                    Column(
+                      children: [
+                        Obx(
+                          () => CustomRadio(
+                              title: 'Hire a Tractor',
+                              value: 'Hire',
+                              groupValue:
+                                  _authController.selectedUserType.value,
+                              onTap: () {
+                                _authController.setSelectedUserType(
+                                    userType: 'Hire');
+                              },
+                              onChanged: (value) {
+                                _authController.setSelectedUserType(
+                                    userType: value);
+                              }),
+                        ),
+                        Obx(
+                          () => CustomRadio(
+                              title: 'Publish my tractor',
+                              value: 'Publish',
+                              groupValue:
+                                  _authController.selectedUserType.value,
+                              onTap: () {
+                                _authController.setSelectedUserType(
+                                    userType: 'Publish');
+                              },
+                              onChanged: (value) {
+                                _authController.setSelectedUserType(
+                                    userType: value);
+                              }),
+                        )
+                      ],
                     ),
-                    Obx(
-                      () => CustomRadio(
-                          title: 'Publish my tractor',
-                          value: 'Publish',
-                          groupValue: _authController.selectedUserType.value,
-                          onTap: () {
-                            _authController.setSelectedUserType(
-                                userType: 'Publish');
-                          },
-                          onChanged: (value) {
-                            _authController.setSelectedUserType(
-                                userType: value);
-                          }),
+
+                    const SizedBox(height: 16),
+                    Align(
+                      alignment: AlignmentDirectional.center,
+                      child: Container(
+                        width: double.infinity,
+                        child: FilledButton(
+                            onPressed: () {
+                              //  create user in firestore
+                              if (_formKey.currentState!.validate() &&
+                                  _authController.selectedUserType.value !=
+                                      null) {
+                                print("Hey");
+                              }
+                            },
+                            style: FilledButton.styleFrom(
+                                backgroundColor: Theme.of(context).primaryColor,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 16)),
+                            child: const Text("Create account")),
+                      ),
                     )
                   ],
                 ),
-
-                const SizedBox(height: 16),
-                Align(
-                  alignment: AlignmentDirectional.center,
-                  child: Container(
-                    width: double.infinity,
-                    child: FilledButton(
-                        onPressed: () {},
-                        style: FilledButton.styleFrom(
-                            backgroundColor: Theme.of(context).primaryColor,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8)),
-                            padding: const EdgeInsets.symmetric(vertical: 16)),
-                        child: const Text("Create account")),
-                  ),
-                )
-              ],
-            ),
-          )),
+              )),
         ),
       ),
     );
