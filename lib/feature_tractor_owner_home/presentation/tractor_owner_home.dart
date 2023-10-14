3d0c0fde09b98face8f4f669afa54e5e50928f6a
KenStarry <starrycodes@gmail.com>
2023-10-14 12:40:21 +0300

    Home Requests

--- lib/feature_tractor_owner_home/presentation/tractor_owner_home.dart
+++ lib/feature_tractor_owner_home/presentation/tractor_owner_home.dart
@@ -11,10 +11,40 @@ class _TractorOwnerHomeState extends State<TractorOwnerHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Requests", style: Theme.of(context).textTheme.titleMedium),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.logout_rounded,
+                color: Theme.of(context).primaryColor,
+              ))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          backgroundColor: Theme.of(context).primaryColorDark.withOpacity(0.7),
+          label: Row(
+            children: [
+              Icon(Icons.location_on_rounded,
+                  color: Theme.of(context).primaryColor),
+              const SizedBox(width: 8),
+              Text("Ping Location",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight:
+                          Theme.of(context).textTheme.bodyMedium!.fontWeight,
+                      fontSize:
+                          Theme.of(context).textTheme.bodyMedium!.fontSize))
+            ],
+          )),
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: SafeArea(
+      body: const SafeArea(
           child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Text("I'm Home"),
       )),
     );
