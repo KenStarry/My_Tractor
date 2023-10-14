7b9052a2443a07adaa2aa959efb60f622ddb359a
KenStarry <starrycodes@gmail.com>
2023-10-14 11:43:48 +0300

    Auth Use Cases

---
+++ lib/core/domain/use_case/auth_use_cases.dart
@@ -0,0 +1,7 @@
+import 'package:my_tractor/core/domain/use_case/create_account.dart';
+
+class AuthUseCases {
+  final CreateAccount createAccount;
+
+  AuthUseCases({required this.createAccount});
+}
--- lib/core/domain/use_case/create_account.dart
+++ lib/core/domain/use_case/create_account.dart
@@ -1,3 +1,17 @@
-class CreateAccount {
+import 'package:my_tractor/core/domain/repository/auth_repository.dart';
+import 'package:my_tractor/di/locator.dart';
 
+import '../model/response_state.dart';
+import '../model/user_model.dart';
+
+class CreateAccount {
+  final repo = locator.get<AuthRepository>();
+
+  Future<void> call({
+    required UserModel userModel,
+    required String password,
+    required Function(ResponseState response, String? error) response,
+  }) async =>
+      repo.createAccount(
+          userModel: userModel, password: password, response: response);
 }
