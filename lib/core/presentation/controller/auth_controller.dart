d58d7b96bf5b03d5301c23caf1f4ff3f4d9bc017
KenStarry <starrycodes@gmail.com>
2023-10-14 11:47:24 +0300

    Create Account Implementation

--- lib/core/presentation/controller/auth_controller.dart
+++ lib/core/presentation/controller/auth_controller.dart
@@ -1,10 +1,22 @@
 import 'package:get/get.dart';
+import 'package:my_tractor/core/domain/use_case/auth_use_cases.dart';
+import 'package:my_tractor/di/locator.dart';
+
+import '../../domain/model/response_state.dart';
+import '../../domain/model/user_model.dart';
 
 class AuthController extends GetxController {
   final selectedUserType = Rxn<String>();
+  final authUseCase = locator.get<AuthUseCases>();
 
   void setSelectedUserType({required String? userType}) =>
       selectedUserType.value = userType;
 
-
+  Future<void> createAccount({
+    required UserModel userModel,
+    required String password,
+    required Function(ResponseState response, String? error) response,
+  }) async =>
+      await authUseCase.createAccount
+          .call(userModel: userModel, password: password, response: response);
 }
