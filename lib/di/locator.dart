208a782778be7d575194c95f82c001dba6ebdfb7
KenStarry <starrycodes@gmail.com>
2023-10-14 11:45:50 +0300

    Register Auth Use Cases

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:my_tractor/core/data/repository/auth_repository_impl.dart';
 import 'package:my_tractor/core/domain/repository/auth_repository.dart';
+import 'package:my_tractor/core/domain/use_case/auth_use_cases.dart';
+import 'package:my_tractor/core/domain/use_case/create_account.dart';
 
 final locator = GetIt.instance;
 
@@ -9,4 +11,6 @@ void invokeDI() {
   locator.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
 
   //  Auth Use cases
+  locator.registerLazySingleton<AuthUseCases>(
+      () => AuthUseCases(createAccount: CreateAccount()));
 }
